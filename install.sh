084d3575b67f41747f46a11d988d5b486e74e69e
Kai Lyons <kai.lyons@kaix.live>
2020-06-01 03:07:31 -0600

    upgrade install script

--- install.sh
+++ install.sh
@@ -3,5 +3,20 @@ if [[ $EUID -ne 0 ]]; then
    echo "This script must be run as root" 
    exit 1
 else
-    cp ./upm /bin/upm
+    if [[ -z $1 ]]; then
+        cp ./upm /bin/upm
+    elif [[ $# -gt 0 ]]; then
+        if [[ $1 = "remove" ]]; then
+            if [[ $# -gt 1 ]]; then
+                echo "incorrect usage!"
+                echo "Usage: install.sh [optional \"uninstall\"]"
+                exit 1
+            fi
+            rm /bin/upm
+        else 
+            echo "incorrect usage!"
+            echo "Usage: install.sh [optional \"uninstall\"]"
+            exit 1
+        fi
+    fi
 fi
